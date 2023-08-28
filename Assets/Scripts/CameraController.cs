70203aa6baa3aa4f49534e5038df3cec325b6f85
Zakhar <zahar.haschuk@gmail.com>
2023-08-28 13:47:13 +0300

    FixedCamera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,33 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //public GameObject Camera1;
-    //public GameObject Camera2;
-    public float sensivity = 8f;
-    public Transform Player;
-    private float rotationX = 0f;
-
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    public Transform player;
+    private Vector3 offset = new Vector3(0, 2.5f, -2);
 
     private void Update()
     {
-        float mouseY = Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
-        float mouseX = Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
-
-        rotationX -= mouseY;
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(rotationX,0f,0f);
-        Player.Rotate(Vector3.up * mouseX);
-
-
-        //if (Input.GetKeyDown("v"))
-        //{
-        //Camera1.SetActive(!Camera1.activeInHierarchy);
-        //Camera2.SetActive(!Camera2.activeInHierarchy);
-        //}
+        transform.position = player.position + offset;
     }
 }
