d51d6f03e5d581475a944f25c4b9fa55392887c9
DEE_615 <kotmurkot89@gmail.com>
2023-08-28 14:14:07 +0300

    comit spawn enemy and none active

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,47 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    public List<GameObject> pooledEnemies;
     public GameObject[] Enemy;
     private int maxEnemies = 30;
     private int currentEnemies = 0;
-    private bool spawning = false; 
-    // Start is called before the first frame update
+    public Transform parentLumberjacks;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && currentEnemies < maxEnemies && !spawning)
-        {
-            spawning = true; 
-            StartCoroutine(SpawnEnemiesCoroutine());
-        }
-    }
-
-    IEnumerator SpawnEnemiesCoroutine()
-    {
+        pooledEnemies = new List<GameObject>();
         for (int i = 0; i < maxEnemies; i++)
         {
             int randomIndex = Random.Range(0, Enemy.Length);
             GameObject selectedObject = Enemy[randomIndex];
 
-            float randX = Random.Range(-3.5f, 5f);
-            float randZ = Random.Range(-5.3f, 4.1f);
-            Vector3 Rposition = new Vector3(randX, 0, randZ);
+            GameObject enemy = Instantiate(selectedObject, Vector3.zero, Quaternion.identity, parentLumberjacks);
+            enemy.SetActive(false);
+            pooledEnemies.Add(enemy); 
+        }
+    }
 
-            Instantiate(selectedObject, Rposition, selectedObject.transform.rotation);
-            currentEnemies++;
-            yield return new WaitForSeconds(0.1f);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && currentEnemies < maxEnemies)
+        {
+            for (int i = 0; i < maxEnemies; i++)
+            {
+                GameObject enemy = GetPooledEnemy(); 
+
+                if (enemy != null)
+                {
+                    float randX = Random.Range(-3.5f, 5f);
+                    float randZ = Random.Range(-5.3f, 4.1f);
+                    Vector3 Rposition = new Vector3(randX, 0, randZ);
+
+                    enemy.transform.position = Rposition;
+                    enemy.SetActive(true);
+                    currentEnemies++;
+                }
+            }
         }
 
-        spawning = false; 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DeactivateAllEnemies();
+        }
+    }
+
+    GameObject GetPooledEnemy()
+    {
+        foreach (GameObject enemy in pooledEnemies)
+        {
+            if (!enemy.activeInHierarchy)
+            {
+                return enemy;
+            }
+        }
+        return null;
+    }
+
+    void DeactivateAllEnemies()
+    {
+        foreach (GameObject enemy in pooledEnemies)
+        {
+            enemy.SetActive(false); 
+        }
+        currentEnemies = 0; 
     }
 }
-
