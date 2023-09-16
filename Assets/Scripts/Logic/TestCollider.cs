4c11008690eaae8ac5d51631cb30c8aa1c0144bf
Zakhar <zahar.haschuk@gmail.com>
2023-09-16 17:21:53 +0300

    EnemyInBiom

---
+++ Assets/Scripts/Logic/TestCollider.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoulEater.Logic {
+    public class TestCollider : MonoBehaviour
+    {
+        [SerializeField] private EnemyController enemyController;
+
+    void Start()
+        {
+
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                enemyController.playerClose = true;
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
