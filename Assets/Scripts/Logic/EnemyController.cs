ef26a7d1d5138129535f323bb9e8081604e922d6
Zakhar <zahar.haschuk@gmail.com>
2023-09-23 15:29:25 +0300

    Return Enemy to start position

--- Assets/Scripts/Logic/EnemyController.cs
+++ Assets/Scripts/Logic/EnemyController.cs
@@ -7,6 +7,7 @@ namespace SoulEater.Logic
 {
     public class EnemyController : MonoBehaviour
     {
+        private Vector3 startPosition;
         //public bool playerClose;
         [SerializeField] float rangeDistance;
         [SerializeField] private Transform playerTransform;
@@ -17,12 +18,21 @@ namespace SoulEater.Logic
             navMeshAgent = GetComponent<NavMeshAgent>();
         }
 
+        private void OnEnable()
+        {
+            startPosition = transform.position;
+        }
+
         void Update()
         {
             float dist = Vector3.Distance(playerTransform.position, transform.position);
             if(rangeDistance >= dist)
             {
                 navMeshAgent.destination = playerTransform.position;
+            }
+            else if(rangeDistance < dist && transform.position != startPosition)
+            {
+                navMeshAgent.destination = startPosition;
             }
 
             //if (playerClose)
