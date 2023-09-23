9666e19cef9488dd84c652c35ba4d7f6e9008ddb
alex <alexasyeyev@gmail.com>
2023-09-23 16:19:49 +0200

    Soul after die
    Soul after die

--- Assets/Scripts/Logic/EnemyController.cs
+++ Assets/Scripts/Logic/EnemyController.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private GameObject SoulPrefab; 
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Sword"))
         {
             gameObject.SetActive(false);
+            Instantiate(SoulPrefab, transform.position, SoulPrefab.transform.rotation);
         }
     }
 }
