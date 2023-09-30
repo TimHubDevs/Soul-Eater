3ed9bbd0fa18100557317d5e2f7cda1004283b6b
DEE_615 <kotmurkot89@gmail.com>
2023-09-30 16:36:32 +0300

    trap enemy

--- Assets/Scripts/Logic/EnemyController.cs
+++ Assets/Scripts/Logic/EnemyController.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private GameObject SoulPrefab; 
+    [SerializeField] private GameObject SoulPrefab;
+    public bool PlayerClose;
+    [SerializeField] private Transform movePosit;
+    public float health = 10f;
+    private NavMeshAgent _navMeshAgent;
     
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+   
+    void Update()
+    {
+
+        if (PlayerClose)
+        {
+            _navMeshAgent.destination = movePosit.position;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Sword"))
@@ -11,5 +31,31 @@ public class EnemyController : MonoBehaviour
             gameObject.SetActive(false);
             Instantiate(SoulPrefab, transform.position, SoulPrefab.transform.rotation);
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (IsAttacking())
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+
+    private bool IsAttacking()
+    {
+
+        return false;
+    }
+
+
+    private void Die()
+    {
+
+        Destroy(gameObject);
     }
 }
